0b4167d89451732cf0450e618a79bbf3133322f8
AdamSpannbauer <spannbaueradam@gmail.com>
2021-08-03 07:44:38 -0400

    update template

--- template/sketch.js
+++ template/sketch.js
@@ -2,28 +2,15 @@
 const canvasW = 512;
 const canvasH = 512;
 
-const saveCoolDown = 60;
-let framesSinceSave = 0;
-
 window.setup = () => {
   createCanvas(canvasW, canvasH, SVG);
-  background(30);
 
   noFill();
 
   stroke(0);
-  strokeWeight(20);
+  strokeWeight(2);
 };
 
 window.draw = () => {
-  framesSinceSave += 1;
-
   ellipse(width / 2, height / 2, 100, 100);
-};
-
-window.mouseClicked = () => {
-  if (framesSinceSave >= saveCoolDown) {
-    save();
-    framesSinceSave = 0;
-  }
 };
